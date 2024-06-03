357b9f70d59f363a62d8b52f5f454e30b2ee496c
Felipe Lourenco <lipelourencosilva@gmail.com>
2024-06-03 11:32:24 -0300

    Inital project setup - springboot

---
+++ src/main/java/com/felipelourenco/course/Springboot3JpaCourseApplication.java
@@ -0,0 +1,13 @@
+package com.felipelourenco.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3JpaCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3JpaCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipelourenco/course/Springboot3JpaCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipelourenco.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3JpaCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
