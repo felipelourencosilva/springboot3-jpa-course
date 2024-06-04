57748a6793089a8461513c57e1f9a86682b9d361
Felipe Lourenco <lipelourencosilva@gmail.com>
2024-06-04 15:45:22 -0300

    Category

---
+++ src/main/java/com/felipelourenco/course/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.felipelourenco.course.services;
+
+import com.felipelourenco.course.entities.Category;
+import com.felipelourenco.course.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> Category = repository.findById(id);
+        return Category.get();
+    }
+}
