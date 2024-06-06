3fd57f577a3508733e670f0ec8d1b009c63d8ee4
Felipe Lourenco <lipelourencosilva@gmail.com>
2024-06-05 23:58:59 -0300

    Exception handling - findById

--- src/main/java/com/felipelourenco/course/services/UserService.java
+++ src/main/java/com/felipelourenco/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.felipelourenco.course.services;
 
 import com.felipelourenco.course.entities.User;
 import com.felipelourenco.course.repositories.UserRepository;
+import com.felipelourenco.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = repository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
